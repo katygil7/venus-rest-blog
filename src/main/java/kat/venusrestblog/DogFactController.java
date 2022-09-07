5f6fb37a7fc80fbf91efb0663c34646f9231fe83
katygil7 <gil.katherine22@gmail.com>
2022-09-07 10:25:48 -0500

    working with  controllers

---
+++ src/main/java/kat/venusrestblog/DogFactController.java
@@ -0,0 +1,20 @@
+package kat.venusrestblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DogFactController {
+    @GetMapping("/dogo")
+    @ResponseBody
+    public String fetchDogFact(){
+        return "Puppies are a pain in the butt!";
+    }
+    @GetMapping("/dogo/{id}")
+    @ResponseBody
+    public String fetchSingleDogFact (@PathVariable long id){
+        return FakeDogFactGateway.fectDogFactById(id);
+    }
+}
---
+++ src/main/java/kat/venusrestblog/FakeDogFactGateway.java
@@ -0,0 +1,17 @@
+package kat.venusrestblog;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class FakeDogFactGateway {
+    public static String fectDogFactById(@PathVariable long id){
+//    fetch the particular dog fact with matching id from database
+        switch ((int) id){
+            case 1:
+                return "Dog don't feel guilty";
+            case 2:
+                return "Their sense of smell is really powerful";
+            default:
+                return "Unknown fact id";
+        }
+    }
+}
