55a8f1aae8494de4ffdcc72d3dd01a80be5a14cd
katygil7 <gil.katherine22@gmail.com>
2022-09-20 14:27:17 -0500

    working on front end

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -1,12 +1,17 @@
+import {isLoggedIn} from "../../auth.js";
+
 export default function Navbar(props) {
-    return `
-        <nav>
-            <a href="/" data-link>Home</a>
-            <a href="/posts" data-link>Posts</a>
-            <a href="/about" data-link>About</a>
-            <a href="/login" data-link>Login</a>
-            <a href="/register" data-link>Register</a>
-            <a href="/me" data-link>About Me</a>
-        </nav>
-    `;
+    let navbar = `<nav>
+                    <a href="/" data-link>Home</a>
+                    <a href="/about" data-link>About</a>`;
+    if (isLoggedIn()) {
+        navbar += `<a href="/posts" data-link>Posts</a>
+                   <a href="/me" data-link>About Me</a>`;
+    } else {
+        navbar +=  `<a href="/login" data-link>Login</a>
+                    <a href="/register" data-link>Register</a>`
+        ;
+    }
+    navbar += `</nav>`;
+    return navbar;
 }
